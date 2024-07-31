1ee7b623cc1f20bcbe1520f65e5c7ac4cee6a3dc
mausam-kumar <mausam_oct@outlook.com>
2024-07-31 08:37:38 +0530

    Initial project setup and install required packages

---
+++ src/app/page.tsx
@@ -0,0 +1,9 @@
+import InitialSetup from "@/components/InitialSetup";
+
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <InitialSetup />
+    </main>
+  );
+}
---
+++ src/components/InitialSetup.tsx
@@ -0,0 +1,5 @@
+const InitialSetup = () => {
+    return <p className="text-red-800">Initial setup</p>
+}
+
+export default InitialSetup
