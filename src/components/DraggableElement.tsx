2ea51ce22ab0a6a27487dca7184a2a633cbeaf4b
Mausam <mausam_oct@outlook.com>
2024-08-01 16:37:43 +0530

    Add funtion to remove element from editor

--- src/components/DraggableElement.tsx
+++ src/components/DraggableElement.tsx
@@ -3,6 +3,7 @@ import React, { useRef } from "react";
 import Button from "./elements/Button";
 import { Element, ElementType } from "../../type";
 import DynamicImage from "./elements/Image";
+import { useEditorStateContext } from "@/context/EditorStateProvider";
 
 interface DraggableElementProps {
   element: Element;
@@ -10,6 +11,7 @@ interface DraggableElementProps {
 }
 
 const DraggableElement: React.FC<DraggableElementProps> = ({ element, onSelect }) => {
+  const { editorState, setEditorState } = useEditorStateContext()
   const ref = useRef<HTMLDivElement>(null);
   const [{ isDragging }, drag] = useDrag({
     type: element.type,
@@ -26,6 +28,14 @@ const DraggableElement: React.FC<DraggableElementProps> = ({ element, onSelect }
 
   drag(drop(ref));
 
+  const removeElementFromEditor = (elementId: string) => {
+    const updatedState = editorState.filter(function ({ id }) {
+        return elementId !== id;
+    })
+
+    setEditorState(updatedState)
+}
+
   return (
     <div
       ref={ref}
@@ -38,8 +48,8 @@ const DraggableElement: React.FC<DraggableElementProps> = ({ element, onSelect }
       onClick={() => onSelect(element.id)}
     >
       {element.type === ElementType.TEXT && <div>{element.content.text}</div>}
-      {element.type === ElementType.IMAGE && <DynamicImage imageURL={element.content.imageURL} />}
-      {element.type === ElementType.BUTTON && <Button type="button" text={element.content.text || "Default Text"} height={Number(element.content.height || 40)} width={Number(element.content.width || 200)} showCloseBtn />}
+      {element.type === ElementType.IMAGE && <DynamicImage imageURL={element.content.imageURL} height={Number(element.content.height || 250)} width={Number(element.content.width || 500)} handleRemove={() => removeElementFromEditor(element.id)} />}
+      {element.type === ElementType.BUTTON && <Button type="button" text={element.content.text || "Default Text"} height={Number(element.content.height || 40)} width={Number(element.content.width || 200)} showCloseBtn handleRemove={() => removeElementFromEditor(element.id)} />}
     </div>
   );
 };
--- src/components/elements/Image.tsx
+++ src/components/elements/Image.tsx
@@ -13,20 +13,21 @@ type ImageProps = {
 }
 
 const DynamicImage: FC<ImageProps> = ({ imageURL = defaultImageURL, handleRemove, alt = "Default alt text", variant = "Desktop", height = 300, width = 400 }) => {
+    console.log({ height, width, imageURL })
     switch (variant) {
         case "Desktop":
             return <figure className="max-w-xl relative" style={{ height, width }}>
-                <Image width={width} height={height} className="h-auto max-w-full rounded-lg" src={imageURL} alt={alt} />
+                <Image width={width} height={height} style={{ height, width }} className="h-auto max-w-full rounded-lg" src={imageURL} alt={alt} />
                 <button onClick={handleRemove} className="rounded-full block p-1 bg-white border text-black text-opacity-60 font-semibold absolute -top-4 z-10 -right-1 h-8 w-8">X</button>
             </figure>
         case "Tablet":
             return <figure className="max-w-lg relative" style={{ height, width }}>
-                <Image width={width} height={height} className="h-auto max-w-full rounded-lg" src={imageURL} alt={alt} />
+                <Image width={width} height={height} style={{ height, width }} className="h-auto max-w-full rounded-lg" src={imageURL} alt={alt} />
                 <button onClick={handleRemove} className="rounded-full block p-1 bg-white border text-black text-opacity-60 font-semibold absolute -top-4 z-10 -right-1 h-8 w-8">X</button>
             </figure>
         case "Mobile":
             return <figure className="max-w-sm relative" style={{ height, width }}>
-                <Image width={width} height={height} className="h-auto max-w-full rounded-lg" src={imageURL} alt={alt} />
+                <Image width={width} height={height} style={{ height, width }} className="h-auto max-w-full rounded-lg" src={imageURL} alt={alt} />
                 <button onClick={handleRemove} className="rounded-full block p-1 bg-white border text-black text-opacity-60 font-semibold absolute -top-4 z-10 -right-1 h-8 w-8">X</button>
             </figure>
 
